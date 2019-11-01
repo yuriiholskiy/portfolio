dd9f88fe1ea12bd43264504cd85ea4e60b449960
Yurii <mors.lviv@gmail.com>
2019-11-01 14:29:02 +0200

    Updates

--- assets/fb03a513e8628709f3f4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{229:function(t,e,n){"use strict";n.r(e);var r={},c=n(0),component=Object(c.a)(r,(function(){var t=this.$createElement;this._self._c;return this._m(0)}),[function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("h1",[this._v("about")])])}],!1,null,null,null);e.default=component.exports}}]);
