c4c4bef02bd521682094a91b04f32916323bc668
Yurii Holskyi <yurii.golskyi@gmail.com>
2020-08-04 22:16:49 +0300

    fix modal padding on mobile screen

--- components/ui/CModal.vue
+++ components/ui/CModal.vue
@@ -106,7 +106,7 @@ export default {
 	position: relative;
 	padding: 1rem 4rem;
 	@media screen and (max-width: map-get($breakpoints , 'tablet-small')) {
-		padding: 1rem 1rem 0 1rem;
+		padding: 1rem 1rem;
 	}
 	@media screen and (max-width: map-get($breakpoints , 'phablet')) {
 		max-height: 200px;
